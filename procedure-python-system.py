7d968c7db6137be29e4ee85b4e26ff5584e3945a
Leticia Moreira Lins <leticiamoreiralins14@gmail.com>
2017-11-11 22:20:41 -0200

    Retirada cometarios

--- procedure-python-system.py
+++ procedure-python-system.py
@@ -11,8 +11,7 @@ try:
             sql = "insert into tb_customer_account (cpf_cnpj, nm_customer, is_active, vl_total)\
                    values ('{}','{}',{},{:.2f})"\
                    .format(i['cpf_cnpj'], i['nm_customer'],
-                           i['is_active'], i['vl_total']) #Inserindo os dados que foram recebidos de um
-                                                          #arquivo json na tabela tb_costumer_account
+                           i['is_active'], i['vl_total']) 
             cursor.execute(sql)
         cursor.close()
         conn.commit()
@@ -25,7 +24,7 @@ try:
     cursor = conn.cursor()
     sql = "select avg(vl_total) from tb_customer_account\
            where vl_total > 560 and\
-           id_customer between 1500 and 2700" #Isso é uma query para tirar a média do vl_total no qual vl_total é maior que 560 e id_customer está entre 1500 e 2700
+           id_customer between 1500 and 2700" 
     cursor.execute(sql)
     result = cursor.fetchone()
     print("A média do vl_total foi R$",result['vl_total'])
@@ -38,7 +37,7 @@ try:
     cursor = conn.cursor()
     sql = "select nm_customer, vl_total from tb_customer_account\
            where vl_total > 560 and\
-           id_customer between 1500 and 2700 order by vl_total desc" #Isso é uma query para tirar a média do vl_total no qual vl_total é maior que 560 e id_customer está entre 1500 e 2700
+           id_customer between 1500 and 2700 order by vl_total desc" 
     cursor.execute(sql)
     result = cursor.fetchall()
     
